99be130307aaa16d4df2a59a08960df0c0d14c7c
akor11lyt <akor1@naver.com>
2022-05-31 00:16:07 +0900

    2022-05-30

---
+++ Example3/js/example.js
@@ -0,0 +1,12 @@
+function buttonClickEvent(event) 
+{
+  switch(event.target.id)
+  {
+    case 'button1':
+      alert('버튼 1');
+      break;
+    case 'button2':
+      alert('버튼2');
+      break;
+  }
+}
