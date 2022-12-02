f072da65465339781964c6c75e65cff8b3deb1e8
akor11lyt <akor1@naver.com>
2022-12-03 00:37:58 +0900

    삭제

--- publisher-page/js/ui.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("nav li").click(function(){
-        //alert("click event");
-        $("nav li").removeClass("on");
-        $(this).addClass("on");
-    });
-});
-
