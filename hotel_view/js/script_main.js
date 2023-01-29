ae6470ef055b34ab11dde53927f4701249473415
yesaroun <akor1@naver.com>
2023-01-29 19:48:04 +0900

    fixed header

--- hotel_view/js/script_main.js
+++ hotel_view/js/script_main.js
@@ -1,8 +1,15 @@
+// 0. a클릭시 위로 튕기는 이벤트제거
+$(document).on('click', 'a[href="#"]', function (e) {
+  e.preventDefault();
+});
+
+
 // 1. approach 영역
 $(window).on('scroll resize', function () {
   var scrollPos = 0;
   scrollPos = $(document).scrollTop();
   fix();
+  fixHeader();
 
   function fix() {
     if (scrollPos > 1250) {
@@ -13,6 +20,15 @@ $(window).on('scroll resize', function () {
     }
     if(scrollPos > 2700) {
       $('.fix .text').removeClass('on');
+    }
+  }
+
+  function fixHeader() {
+    if (scrollPos > 80) {
+      $('header').addClass('on');
+    }
+    else {
+      $('header').removeClass('on');
     }
   }
 });
